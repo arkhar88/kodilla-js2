0a7acb3898242e8a5533477470d79dc1e014bf2e
arkhar88 <arkhar88@gmail.com>
2017-04-23 12:49:56 +0200

    added js method of computing triangle area

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-var name = prompt('Name');
-// alert('helo, ' + name);
-console.log('helo, ' + name);
+// var name = prompt('Name');
+// // alert('helo, ' + name);
+// console.log('helo, ' + name);
 
 /*console.log('make it stop');*/
+
+var base = 6;
+var height = 10;
+
+var compute_triangle = function(a, h) {
+    return (a * h) / 2;
+};
+
+console.log('Triangle area for base: ' + base + ' and height: ' + height + ' equals ' + compute_triangle(base, height));
